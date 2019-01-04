9bbab541f1307093004c8c40a477763d667e7914
许彦峰 <774177933@qq.com>
2019-01-04 15:21:36 +0800

    no message

--- tools/gulpfile.js
+++ tools/gulpfile.js
@@ -4,7 +4,7 @@ const fse = require('fs-extra');
 const path = require('path');
 const ghPages = require('gh-pages');
 
-gulp.task("部署GitHubPages", function () {
+gulp.task("deploy", function () {
 
     let bookDir = path.join(__dirname, "../_book");
     if (fs.existsSync(bookDir)) {
